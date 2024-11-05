16b63b17b26dc361b8b3b9d2a664ca479aaf5a59
W.xavier <wellington222688@gmail.com>
2024-11-04 21:33:36 -0400

    aula php, banco de dados , html, css

--- meusite4/atividade.sql
+++ meusite4/atividade.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT
 	nome VARCHAR(100) NOT NULL 
 );
 
@@ -11,26 +11,26 @@ CREATE TABLE Favorito (
 
 
 INSERT INTO Usuario(nome) values
-('Usuário 1')
-('Usuário 2')
-('Usuário 3')
-('Usuário 4')
-('Usuário 5')
-('Usuário 6')
-('Usuário 7')
-('Usuário 8')
-('Usuário 9')
-('Usuário 10')
-('Usuário 11')
-('Usuário 12')
-('Usuário 13')
-('Usuário 14')
-('Usuário 15')
-('Usuário 16')
-('Usuário 17')
-('Usuário 18')
-('Usuário 19')
-('Usuário 20')
+('Usuário 1'),
+('Usuário 2'),
+('Usuário 3'),
+('Usuário 4'),
+('Usuário 5'),
+('Usuário 6'),
+('Usuário 7'),
+('Usuário 8'),
+('Usuário 9'),
+('Usuário 10'),
+('Usuário 11'),
+('Usuário 12'),
+('Usuário 13'),
+('Usuário 14'),
+('Usuário 15'),
+('Usuário 16'),
+('Usuário 17'),
+('Usuário 18'),
+('Usuário 19'),
+('Usuário 20');
 
 SELECT * FROM Usuario;
 
